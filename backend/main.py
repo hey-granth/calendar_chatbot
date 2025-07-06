724adf6fa9719d5f4ccf4f44877e39abcfbd8131
Granth Agarwal <heygranth@gmail.com>
2025-07-06 17:33:46 +0530

    made function to create google creds json

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import os
 from .agent import agent
+from pathlib import Path
+
+def write_service_account_json():
+    creds_path = Path("creds/service_account.json")
+    creds_path.parent.mkdir(exist_ok=True)
+
+    if not creds_path.exists():
+        creds_json = os.getenv("GOOGLE_CREDS_JSON")
+        if not creds_json:
+            raise RuntimeError("Missing GOOGLE_CREDS_JSON")
+        creds_path.write_text(creds_json)
+
+write_service_account_json()
 
 app = FastAPI()
 
